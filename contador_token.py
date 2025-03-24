36653ac3221572aa9f2abf33471c10413631c875
roniwellington <roniwellington@gmail.com>
2025-03-23 23:29:08 -0300

    usando gemini para analise de sentimentos

--- contador_token.py
+++ contador_token.py
@@ -1,13 +1,13 @@
 import google.generativeai as genai
 
 MODELO_FLASH = "gemini-1.5-flash"
-MODELO_PRO = "gemini-1.5-pro"
+MODELO_NOVA = "gemini-2.0-flash"
 
 CUSTO_ENTRADA_FLASH = 0.075
 CUSTO_SAIDA_FLASH = 0.30
 
-CUSTO_ENTRADA_PRO = 3.5
-CUSTO_SAIDA_PRO = 10.50
+CUSTO_ENTRADA_NOVA = 3.5
+CUSTO_SAIDA_NOVA = 10.50
 
 model_flash = genai.get_model(f"models/{MODELO_FLASH}")
 limites_modelo_flash = {
@@ -17,13 +17,13 @@ limites_modelo_flash = {
 
 print(f"Limites do modelo flash são: {limites_modelo_flash}")
 
-model_pro = genai.get_model(f"models/{MODELO_PRO}")
-limites_modelo_pro = {
-    "tokens_entrada" : model_pro.input_token_limit,
-    "tokens_saida" : model_pro.output_token_limit
+model_nova = genai.get_model(f"models/{MODELO_NOVA}")
+limites_modelo_nova = {
+    "tokens_entrada" : model_nova.input_token_limit,
+    "tokens_saida" : model_nova.output_token_limit
 }
 
-print(f"Limites do modelo pro são: {limites_modelo_pro}")
+print(f"Limites do modelo pro são: {limites_modelo_nova}")
 
 llm_flash = genai.GenerativeModel(
     f"models/{MODELO_FLASH}"
@@ -39,5 +39,5 @@ tokens_resposta = resposta.usage_metadata.candidates_token_count
 custo_total = (tokens_prompt * CUSTO_ENTRADA_FLASH) / 1000000 + (tokens_resposta * CUSTO_SAIDA_FLASH) / 1000000
 print(f"Custo Total U$ Flash: ", custo_total)
 
-custo_total = (tokens_prompt * CUSTO_ENTRADA_PRO) / 1000000 + (tokens_resposta * CUSTO_SAIDA_PRO) / 100.000
+custo_total = (tokens_prompt * CUSTO_ENTRADA_NOVA) / 1000000 + (tokens_resposta * CUSTO_SAIDA_NOVA) / 100.000
 print(f"Custo Total U$ Pro: ", custo_total)
